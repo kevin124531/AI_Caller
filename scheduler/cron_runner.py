0d98ca445a3d847c0ad68f6b13b4a63978dd8b2a
flamethrow12 <kevinkakkad@hotmail.com>
2026-04-28 22:58:54 -0400

    fix time zone for scheduler

--- scheduler/cron_runner.py
+++ scheduler/cron_runner.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime, timezone as dt_timezone
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from config.settings import settings
@@ -12,13 +13,14 @@ def start_scheduler() -> None:
     from scheduler.weekly_job import run_weekly_survey
 
     minute, hour, day, month, day_of_week = settings.scheduler_cron.split()
+    local_tz = datetime.now(dt_timezone.utc).astimezone().tzinfo
     trigger = CronTrigger(
         minute=minute,
         hour=hour,
         day=day,
         month=month,
         day_of_week=day_of_week,
-        timezone="local",
+        timezone=local_tz,
     )
     scheduler.add_job(run_weekly_survey, trigger, id="weekly_survey", replace_existing=True)
     scheduler.start()
